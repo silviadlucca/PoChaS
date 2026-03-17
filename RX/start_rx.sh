04aa6d7e28be6f748d8c85309623f91856e32ae0
Silvia D'Lucca <s.dlucca296@gmail.com>
2026-03-17 10:36:55 +0100

    Update script to change user directory to /home/pi

--- RX/start_rx.sh
+++ RX/start_rx.sh
@@ -4,11 +4,11 @@ sleep 20
 echo "Starting ..."
 echo " --------- "
 
-cd /home/castiello
+cd /home/pi
 
 export DISPLAY=:0
 
-/usr/bin/python3 /home/castiello/PoChaS/RX/GNU_GNSSS_WiFi_v11.py
+/usr/bin/python3 /home/pi/PoChaS/RX/GNU_GNSSS_WiFi_v11.py
 
 echo "-------------------------"
 echo "Ended."
