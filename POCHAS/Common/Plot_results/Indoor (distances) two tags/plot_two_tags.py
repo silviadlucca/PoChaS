31f19620e6e7f8c3f7929a0d5a08728871245850
Silvia D'Lucca <s.dlucca296@gmail.com>
2026-06-30 13:23:29 +0200

    cambios en plot tags

--- POCHAS/Common/Plot_results/Indoor (distances) two tags/plot_two_tags.py
+++ POCHAS/Common/Plot_results/Indoor (distances) two tags/plot_two_tags.py
@@ -157,52 +157,69 @@ ax1.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
 # 2 plot: rssi
 ax2.set_xlabel('Time')
 ax2.set_ylabel('RSSI (dB)')
-ax2.set_title('RSSI vs Time')
+ax2.set_title('RSSI tag vs Time')
 ax2.grid(True)
 ax2.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
 
 plt.tight_layout()
 
-# second figure
-fig_2, (ax3, ax4) = plt.subplots(2, 1, figsize=(8, 8))
+# second figure: xy 
+fig_2, ax3 = plt.subplots(figsize=(8, 6))
 
+all_rssis = positions['1']['rssi'] + positions['2']['rssi']
+if all_rssis and min(all_rssis) == max(all_rssis):
+    vmin, vmax = all_rssis[0] - 1.0, all_rssis[0] + 1.0
+else:
+    vmin, vmax = None, None
 
-# position xy
-for tag in ['1', '2']:
-    x = positions[tag]['x']
-    y = positions[tag]['y']
+# 1st tag
+x1, y1, r1 = positions['1']['x'], positions['1']['y'], positions['1']['rssi']
+if x1:
+    ax3.plot(x1, y1, color='darkred', alpha=0.4, linestyle='-', linewidth=1.5, zorder=1)
+    # colour (red)
+    sc1 = ax3.scatter(x1, y1, c=r1, cmap='Reds', vmin=vmin, vmax=vmax, s=40, marker='o', label='Tag 1', zorder=2)
+    cbar1 = fig_2.colorbar(sc1, ax=ax3, pad=0.01, shrink=0.7)
+    cbar1.set_label('RSSI Tag 1 (dB)')
 
-    ax3.plot(x, y, '-', label=f'Tag Trayectory {tag}',linewidth=2)
-
+# 2nd tag
+x2, y2, r2 = positions['2']['x'], positions['2']['y'], positions['2']['rssi']
+if x2:
+    ax3.plot(x2, y2, color='darkblue', alpha=0.4, linestyle='-', linewidth=1.5, zorder=1)
+    # colour (blue)
+    sc2 = ax3.scatter(x2, y2, c=r2, cmap='Blues', vmin=vmin, vmax=vmax, s=40, marker='o', label='Tag 2', zorder=2)
+    cbar2 = fig_2.colorbar(sc2, ax=ax3, pad=0.08, shrink=0.7)
+    cbar2.set_label('RSSI Tag 2 (dB)')
 
 # anchors
 for idx in range(len(anchors)):
     anchor = anchors[(idx)]
     if anchor != 0:
-        ax3.plot(anchor[0], anchor[1], 'D', markersize=10, color='black')
-ax3.plot([], [], 'D', color='black',label='Anchors')
+        ax3.plot(anchor[0], anchor[1], 'D', markersize=10, color='black', zorder=3)
+ax3.plot([], [], 'D', color='black', label='Anchors')
 
-ax3.set_title('2D Position')
+ax3.set_title('2D Position (Color según RSSI)')
 ax3.set_xlabel('X (m)')
 ax3.set_ylabel('Y (m)')
 ax3.grid()
-ax3.legend()
+ax3.legend(loc='upper left')
 ax3.axis('equal')
+plt.tight_layout()
 
 
-
-# height
+# third figure: height
+fig_3, ax4 = plt.subplots(figsize=(8, 4))
 
 for tag in ['1', '2']:
     z = positions[tag]['z']
     times = positions[tag]['times']
-
-    ax4.plot(times, z, label=f'Height vs Time Tag {tag}')
+    if z:
+        ax4.plot(times, z, label=f'Height vs Time Tag {tag}', linewidth=2)
 
 ax4.set_title('Height vs Time')
 ax4.set_xlabel('Time')
 ax4.set_ylabel('Height (m)')
 ax4.grid()
 ax4.legend()
+plt.tight_layout()
 
 plt.show()
